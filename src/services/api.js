c354cf9defe0e10b8a746a42dd5b57d9a924d918
MVSDias <marcusdias_00@outlook.com>
2025-03-13 17:17:17 -0300

    mudando a porta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://localhost:3002'
 })
 
 // toda vez q for fazer uma requisição intercepto a requisição e coloco uma informação de usuário (userData)
