86ae7b5f8ee0e52f622aec13736db4e77a792655
MVSDias <marcusdias_00@outlook.com>
2025-03-14 22:22:49 -0300

    terminando tela de pagamento

--- src/components/Stripe/CheckoutForm/index.jsx
+++ src/components/Stripe/CheckoutForm/index.jsx
@@ -9,17 +9,19 @@ import '../styles.css';
 import { useCart } from '../../../hooks/CartContext';
 import { api } from '../../../services/api';
 import { toast } from 'react-toastify';
+import BG from '../../../assets/bg-login2.svg'
+
+
 
 export default function CheckoutForm() {
-
-  const { cartProducts, clearCart } = useCart();  
+  const { cartProducts, clearCart } = useCart();
   const {
     state: { clientSecret },
   } = useLocation(); // desmenbrando o location, buscando dentro de state o clientsecret
 
   const stripe = useStripe();
   const elements = useElements();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -41,21 +43,20 @@ export default function CheckoutForm() {
       redirect: 'if_required', // só vai redirecionar quando for requerido
     });
 
-    if (error) { //se deu erro na intenção de pagamento
+    if (error) {
+      //se deu erro na intenção de pagamento
       setMessage(error.message);
       toast.error(error.message);
-
     } else if (paymentIntent && paymentIntent.status === 'succeeded') {
-
       try {
         const products = cartProducts.map((product) => {
-            // faço um map no cardProducts e retorno os produtos no formato que o backend espera(só id, quantidade e preço do produto)
-            return {
-              id: product.id,
-              quantity: product.quantity,
-              price: product.price,
-            };
-          });
+          // faço um map no cardProducts e retorno os produtos no formato que o backend espera(só id, quantidade e preço do produto)
+          return {
+            id: product.id,
+            quantity: product.quantity,
+            price: product.price,
+          };
+        });
 
         const { status } = await api.post(
           '/orders',
@@ -69,7 +70,9 @@ export default function CheckoutForm() {
 
         if (status === 200 || status === 201) {
           setTimeout(() => {
-            navigate(`/complete?payment_intent_client_secret=${paymentIntent.client_secret}`); // passando o clientSecret por queryparams
+            navigate(
+              `/complete?payment_intent_client_secret=${paymentIntent.client_secret}`,
+            ); // passando o clientSecret por queryparams
           }, 2000);
           toast.success('Pedido realizado com sucesso 👌');
           clearCart();
@@ -82,7 +85,9 @@ export default function CheckoutForm() {
         toast.error('Falha no Sistema 🤯! Tente novamente. ');
       }
     } else {
-        toast.error('Falha no Sistema 🤯! Tente novamente. ');
+      navigate(
+        `/complete?payment_intent_client_secret=${paymentIntent.client_secret}`,
+      );
     }
 
     setIsLoading(false);
