8b300e683f7a03f742914912b8038ad0747fc6d3
MVSDias <marcusdias_00@outlook.com>
2025-03-15 15:23:16 -0300

    mudei a forma de navegação das rotas e a fonte do vscode

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { router } from './routes';
+
 import GlobalStyles from './styles/globalStyles';
 import AppProvider from './hooks';
 import { Elements } from '@stripe/react-stripe-js';
 import stripePromisse from './config/stripeConfig';
 import { ThemeProvider } from 'styled-components';
 import { standardTheme } from './styles/themes/standard';
+import { Router } from './routes';
 
 
 createRoot(document.getElementById('root')).render(
@@ -17,7 +18,9 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={standardTheme}>
       <AppProvider>
         <Elements stripe={stripePromisse}>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+          <Router />
+          </BrowserRouter>
           <GlobalStyles />
           <ToastContainer autoClose={2000} theme="colored" />
         </Elements>
